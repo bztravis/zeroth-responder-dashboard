d4d58ceb4c52a3a78660690796c624f4b7f3dbf6
Brian Travis <bztravis88@gmail.com>
2023-10-29 11:56:27 -0700

    Add everything

--- app/page.js
+++ app/page.js
@@ -3,22 +3,43 @@
 import { useState, useEffect } from 'react'
 import Caller from '@/components/Caller'
 import styles from './page.module.css'
+import { createClient } from '@supabase/supabase-js'
 
 export default function Home() {
-  const [callers, setCallers] = useState([
-    {
-      priority: 2,
-      summary: 'My cat is stuck in the tree',
-    },
-    {
-      priority: 1,
-      summary: 'House on fire',
-    },
-  ])
+  const [callers, setCallers] = useState([])
+  const supabase = createClient(
+    'https://qyywhmcascmnzzrxfagn.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5eXdobWNhc2Ntbnp6cnhmYWduIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg1OTk2NjYsImV4cCI6MjAxNDE3NTY2Nn0.1n2TZy24Ee1ydjpzPreCjMfeV6vMx9POu6DvhvNUOCw'
+  )
+  useEffect(() => {
+    const initialFetch = async () => {
+      let { data: callers, error } = await supabase.from('callers').select('*')
+      console.log('callers', callers)
+      setCallers(callers)
+    }
+    initialFetch()
+  }, [])
+
+  useEffect(() => {
+    console.log('callers', callers)
+  }, [callers])
+
+  const callersSub = supabase
+    .channel('custom-insert-channel')
+    .on(
+      'postgres_changes',
+      { event: 'INSERT', schema: 'public', table: 'callers' },
+      (payload) => {
+        console.log('Change received!', payload)
+        setCallers((prev) => [...prev, payload])
+      }
+    )
+    .subscribe()
+
   return (
     <main className={styles.container}>
       {callers.map((elem) => (
-        <Caller data={elem}></Caller>
+        <Caller data={elem.data} setCallers={setCallers}></Caller>
       ))}
     </main>
   )
--- components/Caller.js
+++ components/Caller.js
@@ -1,5 +1,22 @@
 import styles from '@/styles/components/Caller.module.scss'
 
-export default function Caller({ data }) {
-  return <div className={styles.container}>{data.summary}</div>
+export default function Caller({ data, setCallers }) {
+  return (
+    <div
+      className={styles.container}
+      style={{
+        background: data.priority === 1 ? '#FF8080' : '#FFA680',
+      }}
+    >
+      <p>{data.summary_points}</p>
+      <button
+        onClick={() =>
+          setCallers((prev) => prev.filter((elem) => elem.data.id !== data.id))
+        }
+      >
+        Resolve
+      </button>
+      <button>Call</button>
+    </div>
+  )
 }
